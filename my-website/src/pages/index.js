efdd849ca9fc8336aa2a8b9b696f09afe902aec6
Belinda Salas <2022238@utsh.edu.mx>
2024-11-27 18:43:08 -0600

    [ fix] belinda

--- my-website/src/pages/index.js
+++ my-website/src/pages/index.js
@@ -20,7 +20,7 @@ function HomepageHeader() {
           <Link
             className="button button--secondary button--lg"
             to="/docs/intro">
-            Docusaurus Tutorial - 150  min 
+            Docusaurus Tutorial arquitectura 
           </Link>
         </div>
       </div>
