9bbae9b6ededb802a16a0c464aa0073cca0f3225
amillaly <leydif840@gmail.com>
2024-11-27 23:47:04 -0600

    [fix] Reacomode el concepto de los patrones, el texto que coloque me parece mas entendible

--- my-website/src/pages/index.js
+++ my-website/src/pages/index.js
@@ -18,12 +18,12 @@ function HomepageHeader() {
         <p className="hero__subtitle">{siteConfig.tagline}</p>
         <div className={styles.buttons}>
         <p className={styles.cardText}>
-        Los patrones de diseño de comportamiento se caracterizan por un complejo 
+        Los patrones de comportamiento tratan con algoritmos y la asignación 
+        de responsabilidades entre objetos y se caracterizan por un complejo 
         control de flujo, el cual es difícil manejar en tiempo de ejecución, 
         por lo que se provoca que el enfoque se cambie lejos del control de flujo 
         para concentrarse exclusivamente en la manera en la que los objetos se comunican.
-        Los patrones de comportamiento tratan con algoritmos y la asignación de responsabilidades
-        entre objetos.
+      
         </p>
         
         </div>
