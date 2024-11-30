cbf666adcbbd83792b54e69570ce347808bb75db
amillaly <leydif840@gmail.com>
2024-11-29 18:43:46 -0600

    [fix] alineacion de texto concepto de patrones

--- my-website/src/pages/index.js
+++ my-website/src/pages/index.js
@@ -16,32 +16,21 @@ function HomepageHeader() {
           {siteConfig.title}
         </Heading>
         <p className="hero__subtitle">{siteConfig.tagline}</p>
-        <div className={styles.buttons}>
-        <p className={styles.cardText}>
-        Los patrones de comportamiento tratan con algoritmos y la asignación 
-        de responsabilidades entre objetos y se caracterizan por un complejo 
-        control de flujo, el cual es difícil manejar en tiempo de ejecución, 
-        por lo que se provoca que el enfoque se cambie lejos del control de flujo 
-        para concentrarse exclusivamente en la manera en la que los objetos se comunican.
-        </p>
-        <p className={styles.cardText}>
-        Características clave de los patrones de comportamiento:
-
-        1. Desacoplamiento de objetos: Permiten que los objetos 
-        interactúen sin conocer los detalles internos de los otros, 
-        facilitando cambios sin afectar al sistema completo.
-
-        2. Facilitación de la comunicación: Establecen maneras estándar
-         para que los objetos se comuniquen, asegurando cooperación sin 
-         depender directamente unos de otros.
-
-        3. Encapsulación de algoritmos y comportamiento: Permiten encapsular
-         algoritmos y comportamientos para su reutilización en diferentes contextos.
-
-        4. Distribución de responsabilidades: Promueven la asignación clara 
-        de responsabilidades entre clases y objetos, mejorando la claridad y
-         mantenibilidad del diseño.
-        </p>
+        <div className={styles.textContainer}>
+          <p className={styles.cardText}>
+            Los patrones de comportamiento tratan con algoritmos y la asignación 
+            de responsabilidades entre objetos y se caracterizan por un complejo 
+            control de flujo, el cual es difícil manejar en tiempo de ejecución, 
+            por lo que se provoca que el enfoque se cambie lejos del control de flujo 
+            para concentrarse exclusivamente en la manera en la que los objetos se comunican.
+          </p>
+          <p className={styles.cardText}>
+            Características clave de los patrones de comportamiento:
+            <br />1. Desacoplamiento de objetos: Permiten que los objetos interactúen sin conocer los detalles internos de los otros, facilitando cambios sin afectar al sistema completo.
+            <br />2. Facilitación de la comunicación: Establecen maneras estándar para que los objetos se comuniquen, asegurando cooperación sin depender directamente unos de otros.
+            <br />3. Encapsulación de algoritmos y comportamiento: Permiten encapsular algoritmos y comportamientos para su reutilización en diferentes contextos.
+            <br />4. Distribución de responsabilidades: Promueven la asignación clara de responsabilidades entre clases y objetos, mejorando la claridad y mantenibilidad del diseño.
+          </p>
         </div>
       </div>
     </header>
